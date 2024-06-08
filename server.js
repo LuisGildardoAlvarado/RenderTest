7ef70a13b32e0f40ac01002c0c64dde716262396
LuisGildardoAlvarado <luisgil9900@gmail.com>
2024-06-07 18:32:01 -0700

    ssl remove

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import express from "express";
+import pg from "pg";
 import path from "path";
 import { fileURLToPath } from "url";
+import { config } from "dotenv";
 
+config();
 const app = express();
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -10,6 +17,11 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/ping", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  return res.json(result.rows[0]);
 });
 
 const PORT = process.env.PORT || 3000;
